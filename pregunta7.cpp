cf878876c4ebf67f1275795037346b6db2bed540
RayzaRodriguez <rayza12_36@hotmail.com>
2017-04-21 19:53:12 -0500

    Add files via upload

---
+++ pregunta7.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void inter(int n1,int n2)
+{
+    int temp=0;
+    temp=n1;
+    n1=n2;
+    n2=temp;
+    cout<<n1<<endl;
+    cout<<n2<<endl;
+}
+
+int main()
+{
+    int n1,n2;
+    cout<<"Ingrese numero 1: ";
+    cin>>n1;
+    cout<<"Ingrese numero 2: ";
+    cin>>n2;
+    inter(n1,n2);
+
+    return 0;
+
+}
